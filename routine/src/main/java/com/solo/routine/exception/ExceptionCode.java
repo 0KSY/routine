496c046400a4ae3bc1c1ad11527bf5bc62502286
0KSY <uryhon7@gmail.com>
2025-09-17 10:37:07 +0900

    공통 기능 구현

---
+++ routine/src/main/java/com/solo/routine/exception/ExceptionCode.java
@@ -0,0 +1,17 @@
+package com.solo.routine.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    ;
+
+    @Getter
+    private int status;
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
